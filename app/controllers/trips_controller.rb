13157cf89c0fe3cbcbb1d2f34f6df54ed8af22ce
Elsa Gounot <elsa.gounot@gmail.com>
2018-03-19 17:11:23 +0100

    new and create method for trip

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,17 @@
 class TripsController < ApplicationController
+
+  def new
+    @trip = Trip.new
+  end
+
+  def create
+    @trip = Trip.create(trip_params)
+    redirect_to trip_path(@trip)
+  end
+
+  private
+
+  def trip_params
+    params.require(:trip).permet(:destination, :photo, :start_date, :end_date, :description, :name)
+  end
 end
