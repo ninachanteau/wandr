8556d54ebc560d67e1adae3ca63128bbaa3c4bd9
Marion Bergeret <bergeret.marion@gmail.com>
2018-03-20 16:19:53 +0100

    edit status buttons added to transpo index

--- app/controllers/transportations_controller.rb
+++ app/controllers/transportations_controller.rb
@@ -7,6 +7,29 @@ def index
   @transportations = @trip.transportations.all
 end
 
+def edit
+  @transportation = Transportation.find(params[:id])
+end
 
+def update
+  @trip = Trip.find(params[:trip_id])
+  @transportation = Transportation.find(params[:id])
+  @transportation.update(status: params[:status])
+  @transportation.save
+  redirect_to trip_transportations_path(@trip)
+end
+
+def destroy
+  @transportation = Transportation.find(params[:id])
+  @transportation.destroy
+  redirect_to trip_transportations_path(@trip)
+end
+
+private
+
+def transpo_params
+  params.require(:transportation).permit(:status)
+
+end
 
 end
