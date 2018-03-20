9184c352976045fdebea73168f61a197198640c6
Nina Chanteau <nina.chanteau@hec.edu>
2018-03-20 18:52:55 +0100

    default picture and seed saving

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -2,13 +2,22 @@ class PhotoUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
 
   def default_url(*args)
-    ActionController::Base.helpers.asset_path(["default.jpg"].compact.join('_'))
+    if self.model.class == User
+      ActionController::Base.helpers.asset_path(["default-user.png"].compact.join('_'))
+    elsif self.model.class == Trip
+      ActionController::Base.helpers.asset_path(["default-trip.jpg"].compact.join('_'))
+    elsif self.model.class == Accommodation
+      ActionController::Base.helpers.asset_path(["default-trip.jpg"].compact.join('_'))
+    elsif self.model.class == Restaurant
+      ActionController::Base.helpers.asset_path(["default-trip.jpg"].compact.join('_'))
+    elsif self.model.class == Activity
+      ActionController::Base.helpers.asset_path(["default-trip.jpg"].compact.join('_'))
+    end
   end
 
   process eager: true  # Force version generation at upload time.
 
   process convert: 'jpg'
-
 
   version :thumnail do
     resize_to_fit 256, 256
