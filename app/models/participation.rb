11cc8357d910a94534d858a7ead7ce9084da752f
Kenza Aboudrar <kenza.aboudrar@hec.edu>
2018-03-20 16:53:27 +0100

    added validation of uniqueness of email

--- app/models/participation.rb
+++ app/models/participation.rb
@@ -5,6 +5,7 @@ class Participation < ApplicationRecord
   has_many :accommodations
   has_many :restaurants
   has_many :activities
+  validates_uniqueness_of :email
   has_secure_token
   after_create :send_join_trip_email
 
