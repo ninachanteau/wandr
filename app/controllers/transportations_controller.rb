a982767ec101957f73d827381bce35e63cf85036
Marion Bergeret <bergeret.marion@gmail.com>
2018-03-20 17:06:17 +0100

    beginning of new form for transpo

--- app/controllers/transportations_controller.rb
+++ app/controllers/transportations_controller.rb
@@ -7,6 +7,20 @@ def index
   @transportations = @trip.transportations.all
 end
 
+def new
+  @trip = Trip.find(params[:trip_id])
+  @transportation = Transportation.new
+end
+
+def create
+  @trip = Trip.find(params[:trip_id])
+  @transportation = Transportation.create(transpo_params)
+  @transportation.trip = @trip
+  @trip.save
+  redirect_to trip_transportations_path(@trip)
+end
+
+
 def edit
   @transportation = Transportation.find(params[:id])
 end
