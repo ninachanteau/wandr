aae3054e2940c192f7e9356af46f23eb8c34c071
Nina Chanteau <nina.chanteau@hec.edu>
2018-03-20 10:23:33 +0100

    link trip avec participation

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -5,6 +5,7 @@ class Trip < ApplicationRecord
   has_many :restaurants, through: :participations
   has_many :activities, through: :participations
   mount_uploader :photo, PhotoUploader
+  validates :photo, presence: true
 
   scope :future, -> { where('end_date >= ?', Date.today) }
   scope :past, -> { where('end_date < ?', Date.today) }
