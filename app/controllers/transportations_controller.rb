f8442e9ca7a16dbf9f86b8de32f5cb9963265150
Marion Bergeret <bergeret.marion@gmail.com>
2018-03-20 12:44:22 +0100

    transportation index work in progress

--- app/controllers/transportations_controller.rb
+++ app/controllers/transportations_controller.rb
@@ -2,6 +2,7 @@ class TransportationsController < ApplicationController
 
 def index
   @trip = Trip.find(params[:trip_id])
+  @participation = Participation.where(trip_id: @trip.id, user_id: current_user.id)
   @transportations = @trip.transportations.all
 end
 
