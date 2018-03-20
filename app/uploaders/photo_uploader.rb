d0c806560ed2a1a17a6abea4dd7bae7572fafeb0
Nina Chanteau <nina.chanteau@hec.edu>
2018-03-20 12:24:40 +0100

    wishlist

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,16 +1,21 @@
 class PhotoUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
 
-    process eager: true  # Force version generation at upload time.
+  # def default_url(*args)
+  #   ActionController::Base.helpers.asset_path(["default.jpg"].compact.join('_'))
+  # end
 
-    process convert: 'jpg'
+  process eager: true  # Force version generation at upload time.
 
-    version :thumnail do
-      resize_to_fit 256, 256
-    end
+  process convert: 'jpg'
 
-    version :bright_face do
-      cloudinary_transformation effect: "brightness:30", radius: 20,
-        width: 150, height: 150, crop: :thumb, gravity: :face
-    end
+
+  version :thumnail do
+    resize_to_fit 256, 256
+  end
+
+  version :bright_face do
+    cloudinary_transformation effect: "brightness:30", radius: 20,
+      width: 150, height: 150, crop: :thumb, gravity: :face
+  end
 end
