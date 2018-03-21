ede54621dd4377f039a9e2a7b3fa10fb6dc38681
Kenza Aboudrar <kenza.aboudrar@hec.edu>
2018-03-21 13:49:07 +0100

    added ifs in scraping

--- app/controllers/accommodations_controller.rb
+++ app/controllers/accommodations_controller.rb
@@ -20,22 +20,22 @@ class AccommodationsController < ApplicationController
     html_content = open(url).read
     doc = Nokogiri::HTML(html_content)
     name_array = doc.search('.heading_title').map { |element| element.text.strip.to_s }
-    @accommodation.name = name_array[0]
+    @accommodation.name = name_array[0] if name_array[0].present?
     street_array = doc.search('.street-address').map { |element| element.text.strip.to_s }
     city_array = doc.search('.locality').map { |element| element.text.strip.to_s }
     # country_array = doc.search('.country-name').map { |element| element.text.strip.to_s }
     # +" "+country_array[0]
-    @accommodation.address = street_array[0]+","+city_array[0]
+    @accommodation.address = street_array[0]+","+city_array[0] if street_array[0].present? && city_array[0]
     phone_array = doc.search('.blEntry span').map { |element| element.text.strip.to_s }
-    @accommodation.phone_number = phone_array[5]
+    @accommodation.phone_number = phone_array[5] if phone_array[5].present?
     img_array = doc.search('.page_images img').map{ |i| i['src'] }
-    @accommodation.remote_photo_url = img_array[1]
+    @accommodation.remote_photo_url = img_array[1] if img_array[1].present?
     if @acommodation.save
-      if Accommodation.where(@accommodation.name).where(@accommodation.trip).count > 1
-        redirect_to trip_accommodations
-      else
+      # if Accommodation.where(@accommodation.name).where(@accommodation.trip).count > 1
+      #   redirect_to trip_accommodations
+      # else
       redirect_to edit_accommodation_path(@accommodation)
-      end
+      # end
     else
       render 'trips/index'
     end
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -20,18 +20,18 @@ class RestaurantsController < ApplicationController
     html_content = open(url).read
     doc = Nokogiri::HTML(html_content)
     name_array = doc.search('.heading_title').map { |element| element.text.strip.to_s }
-    @restaurant.name = name_array[0]
+    @restaurant.name = name_array[0] if name_array[0].present?
     street_array = doc.search('.street-address').map { |element| element.text.strip.to_s }
     city_array = doc.search('.locality').map { |element| element.text.strip.to_s }
     # country_array = doc.search('.country-name').map { |element| element.text.strip.to_s }
     # +" "+country_array[0]
-    @restaurant.address = street_array[0]+","+city_array[0]
+    @restaurant.address = street_array[0]+","+city_array[0] if street_array[0].present? && city_array[0]
     description_array = doc.search('.additional_info .content').map { |element| element.text.strip.to_s }
-    @restaurant.description = description_array.last
+    @restaurant.description = description_array.last if description_array.last.present?
     phone_array = doc.search('.blEntry span').map { |element| element.text.strip.to_s }
-    @restaurant.phone_number = phone_array[5]
+    @restaurant.phone_number = phone_array[5] if phone_array[5].present?
     img_array = doc.search('.page_images img').map{ |i| i['src'] }
-    @restaurant.remote_photo_url = img_array[1]
+    @restaurant.remote_photo_url = img_array[1] if img_array[1].present?
     if @restaurant.save
       # if Restaurant.where(name: name_array[0]) == []
         redirect_to edit_restaurant_path(@restaurant)
