068d0915dd9ef9350e02a4610d3fd5e88b8f09d2
Kenza Aboudrar <kenza.aboudrar@hec.edu>
2018-03-21 14:22:21 +0100

    use @current_participation for map

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -6,6 +6,7 @@ class RestaurantsController < ApplicationController
   def index
     @trip = Trip.find(params[:trip_id])
     @participation = Participation.where(trip_id: @trip.id, user_id: current_user.id)
+    @current_participation = Participation.where(trip_id: @trip.id, user_id: current_user.id)
     @avatar = current_user.avatar
     @restaurants = @trip.restaurants.all
   end
