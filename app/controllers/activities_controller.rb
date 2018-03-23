81b502cf5bcc4ceb1d725a5986eac703cc858fba
Kenza Aboudrar <kenza.aboudrar@hec.edu>
2018-03-23 13:21:50 +0100

    no duplication anymore - to be checked with show

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -4,8 +4,19 @@ require "nokogiri"
 class ActivitiesController < ApplicationController
 
   def index
+    # @trip = Trip.find(params[:trip_id])
+    # @activities = Activity.where(trip_id: @trip.id)
+    # @activity = Activity.new
     @trip = Trip.find(params[:trip_id])
-    @activities = Activity.where(trip_id: @trip.id)
+    @current_participation = Participation.where(trip_id: @trip.id, user_id: current_user.id).first
+    @my_activities = @current_participation.activities
+    @trip_participants =  @trip.participations
+    @all_reservations = Activity.where(trip_id: @trip.id)
+    @all_activities = []
+    @trip.all_activities.each do |key, _value|
+      @all_activities << @all_reservations.where(name:key[0], date: key[1]).first unless @all_reservations.where(name:key[0], date: key[1]).nil?
+    end
+    @activities = @all_activities.reject { |resa| resa unless (resa.same_reservation & @my_activities).empty? }
     @activity = Activity.new
   end
 
