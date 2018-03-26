812e4db7aca767af7d6a82d331387f97e0a0ff36
Nina Chanteau <nina.chanteau@hec.edu>
2018-03-26 15:45:18 +0200

    start calendar improvement

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -4,31 +4,51 @@ import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
 
 flatpickr("#trip_start_date", {
   altInput: true,
+  altFormat: "F j, Y",
+  dateFormat: "Y-m-d",
   'static': true,
+  minDate: "today",
   plugins: [new rangePlugin({ input: "#trip_end_date" })]
 })
 
 flatpickr("#transportation_departure_date", {
   altInput: true,
+  altFormat: "F j, Y",
+  dateFormat: "Y-m-d",
   enableTime: true,
   'static': true,
+  // minDate: ,
+  // maxDate: ,
   plugins: [new rangePlugin({ input: "#transportation_arrival_date" })]
-})
-
-flatpickr("#restaurant_date", {
-  altInput: true,
-  enableTime: true,
-  'static': true,
 })
 
 flatpickr("#accommodation_start_date", {
   altInput: true,
+  altFormat: "F j, Y",
+  dateFormat: "Y-m-d",
   enableTime: true,
   'static': true,
+  // minDate: ,
+  // maxDate: ,
   plugins: [new rangePlugin({ input: "#accommodation_end_date" })]
+})
+
+flatpickr("#restaurant_date", {
+  altInput: true,
+  altFormat: "F j, Y",
+  dateFormat: "Y-m-d",
+  enableTime: true,
+  'static': true,
+  // minDate: ,
+  // maxDate:
 })
 
 flatpickr("#activity_date", {
   altInput: true,
-  enableTime: true
+  altFormat: "F j, Y",
+  dateFormat: "Y-m-d",
+  enableTime: true,
+  'static': true,
+  // minDate: ,
+  // maxDate:
 })
