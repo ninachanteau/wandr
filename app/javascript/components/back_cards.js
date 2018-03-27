01c155026cbfeb339ec07aeee960fb56112a71f5
Elsa Gounot <elsa.gounot@gmail.com>
2018-03-27 23:34:42 +0200

    details and transpo cards

---
+++ app/javascript/components/back_cards.js
@@ -0,0 +1,12 @@
+// const plusItem = document.querySelectorAll("i.plus-return");
+// const backCard = document.querySelectorAll(".card-back");
+// plusItem.forEach((element) => {
+//   element.addEventListener("mouseover", (event) => {
+//     const selectedCard = event.currentTarget.querySelector("index-card");
+//     plusItem.forEach((elem) => {
+//       elem.querySelector("index-card").style.display = "none";
+//     });
+//     backCard.style.display = "block";
+//   });
+// });
+
