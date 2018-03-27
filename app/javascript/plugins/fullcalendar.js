c1354ac9df91efbd1542c99f24d549637e4e0d45
Marion Bergeret <bergeret.marion@gmail.com>
2018-03-27 16:06:01 +0200

    bug fixed

--- app/javascript/plugins/fullcalendar.js
+++ app/javascript/plugins/fullcalendar.js
@@ -6,46 +6,50 @@ function displayCalendar() {
   const calendarElement = document.getElementById('calendar');
   if (calendarElement) {
     const events = JSON.parse(calendarElement.dataset.events);
-      $('#calendar').fullCalendar({
-          locale: 'en',
-          firstDay: 1,
-          height: 450,
-          header: {
-            left:   'title',
-            center: 'months,weeks,days'
+    $('#calendar').fullCalendar({
+        locale: 'en',
+        firstDay: 1,
+        height: 450,
+        header: {
+          left:   'title',
+          center: 'months,weeks,days'
+        },
+        buttonText: {
+          today:    'today',
+          month:    'M',
+          week:     'W',
+          day:      'D'
+        },
+        views: {
+          months: {
+            type: 'month'
           },
-          buttonText: {
-            today:    'today',
-            month:    'M',
-            week:     'W',
-            day:      'D'
+          weeks: {
+            type: 'agendaWeek',
+            titleFormat: 'MMM D',
+            columnHeaderFormat: 'ddd D',
+            scrollTime: "10:00:00",
+            nowIndicator: true,
+            slotDuration:'01:00:00',
+            slotLabelInterval: "02:00"
           },
-          views: {
-            months: {
-              type: 'month'
-            },
-            weeks: {
-              type: 'agendaWeek',
-              titleFormat: 'MMM D',
-              columnHeaderFormat: 'ddd D',
-              scrollTime: "10:00:00",
-              nowIndicator: true,
-              slotDuration:'01:00:00',
-              slotLabelInterval: "02:00"
-            },
-            days: {
-              type: 'agenda',
-              duration: { days: 1 },
-              titleFormat: 'MMMM D',
-              scrollTime: "10:00:00",
-              nowIndicator: true,
-              slotDuration:'01:00:00',
-              slotLabelInterval: "02:00"
-            }
-          },
-          events: events
-        });
+          days: {
+            type: 'agenda',
+            duration: { days: 1 },
+            titleFormat: 'MMMM D',
+            scrollTime: "10:00:00",
+            nowIndicator: true,
+            slotDuration:'01:00:00',
+            slotLabelInterval: "02:00"
+          }
+        },
+        events: events
+      });
+
+    if (events.length !== 0 ) {
       $('#calendar').fullCalendar('gotoDate', new Date(events[0].start));
+    }
+
   }
 }
 
