dafd73ce95010edaf78db85f2ea9a06d1fdcbd9f
Nina Chanteau <nina.chanteau@hec.edu>
2018-03-29 17:14:16 +0200

    fix map zoom

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -15,10 +15,10 @@ if (mapElement) { // don't try to build a map if there's no div#map to inject in
     map.setZoom(2);
   } else if (markers.length === 1) {
     map.setCenter(markers[0].lat, markers[0].lng);
-    map.setZoom(14);
+    map.setZoom(11);
   } else {
     // map.fitLatLngBounds(markers);
-    map.setCenter(markers[1].lat, markers[1].lng);
+    map.setCenter(markers[5].lat, markers[5].lng);
     map.setZoom(11);
   }
 }
