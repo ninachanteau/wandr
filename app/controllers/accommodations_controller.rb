c61f711cc4bf38097a276368dee2b41b11ebcea1
Marion Bergeret <bergeret.marion@gmail.com>
2018-03-29 11:35:54 +0200

    ajax navigation corrected

--- app/controllers/accommodations_controller.rb
+++ app/controllers/accommodations_controller.rb
@@ -60,10 +60,7 @@ class AccommodationsController < ApplicationController
     @accommodation = Accommodation.find(params[:id])
     @accommodation.same_reservation.each { |resa| resa.update(status: params[:status]) }
     # @accommodation.update(status: params[:status])
-    respond_to do |format|
-      format.html { redirect_to trip_accommodations_path(@trip) }
-      format.js { j render 'index' }
-    end
+    redirect_to trip_accommodations_path(@trip)
   end
 
   def destroy
